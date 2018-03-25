197936c87a4e4b0a3e2f6cb57ebbb07e22fcc9e4
Adam Bednarek <adamjb51@gmail.com>
2018-03-25 12:02:22 -0400

    Improve readability of constructing class
    There's a character limit we want to keep per line, so defining new
    lines of each argument helps with readability.

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,15 @@ import homework.DogExpanded
 // This is main, the entry point of a Java/Kotlin Application
 fun main(args: Array<String>) {
     // See how both Dog classes are Constructed the same way:
-    val expandedDog = DogExpanded(name = "Maya", breed = "Bull Terrior", color = "Blue", barkSound = "Woof")
-    
-    val myDogMaya = Dog(name = "Maya", breed = "Bull Terrior", color = "Blue", barkSound = "Woof")
+    val expandedDog = DogExpanded(
+            name = "Maya",
+            breed = "Bull Terrior",
+            color = "Blue",
+            barkSound = "Woof")
+
+    val myDogMaya = Dog(
+            name = "Maya",
+            breed = "Bull Terrior",
+            color = "Blue",
+            barkSound = "Woof")
 }
