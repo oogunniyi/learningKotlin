99accf680bc751a8f445900b89cde7c0d3b38d4c
Adam Bednarek <adamjb51@gmail.com>
2018-03-25 11:40:33 -0400

    Print Fib Sequence

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,10 @@
+import homework.printFibSequence
+
 // This is main, the entry point of a Java/Kotlin Application
 fun main(args: Array<String>) {
-    TODO("Run Assignment functions here")
+    // Print out various sequences to verify things work.
+    printFibSequence(nThPosition = 0)
+    printFibSequence(nThPosition = 1)
+    printFibSequence(nThPosition = 2)
+    printFibSequence(nThPosition = 10)
 }
--- src/homework/HomeWork.kt
+++ src/homework/HomeWork.kt
@@ -6,7 +6,7 @@ Programming exercises:
  * Write a function to print a list of even numbers between a certain range:
  * printEvenNumbers(start, finish)
  * Write a function to print a fib sequence up to specified number position
- * printFib(number)
+    * printFib(number)
  * Create a class called “Dog” with properties / methods:
      * val name
      * val breed
@@ -17,3 +17,35 @@ Programming exercises:
      * def run() -> prints dog is running, make isHungry to true
      * def eat() -> if Hungry, print Dog eats, if not hungry, print Dog refuses to eat.
  */
+
+/**
+ * Prints the fibonacci sequence up to the nth position
+ *
+ * @param nThPosition 0 based positioning of the Fib Sequence
+ */
+fun printFibSequence(nThPosition: Int) {
+    // The first
+    val fibSequence = mutableListOf(0)
+    if (nThPosition == 0) {
+        println(fibSequence)
+        return
+    }
+
+    // The sequence now contains 0, 1
+    fibSequence.add(1)
+    // We store our iterated value in n, and will go up to the nth position
+    var n = 1
+    while (n < nThPosition) {
+        // The last Fib Number is the previous element in the list
+        val lastFibNumber = fibSequence.get(n-1)
+        // The Current Fib number is the current element in the list
+        val currentFibNumber = fibSequence.get(n)
+        // Add the Previous number and the current number, then append it to the list
+        fibSequence.add(lastFibNumber + currentFibNumber)
+
+        // Increment n for the next position
+        n++
+    }
+
+    println(fibSequence)
+}
