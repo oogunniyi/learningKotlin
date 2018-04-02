b7cdb17d86ee1586a7031e79544f8bed46de0230
Unknown <oogunniyi@schoology.com>
2018-04-02 15:08:34 -0400

    blah

---
+++ src/Student.kt
@@ -0,0 +1,7 @@
+class Student(var gender: String, var favoriteSubject: String, var firstName: String, var numericGrade: Int, var age: Int, var lastName: String) {
+
+    fun getLetterGrade(): String { // TODO:
+
+    }
+
+}
---
+++ src/Teacher.kt
@@ -0,0 +1,6 @@
+class Teacher(var firstName: String, var age: Int=0, var students: Array<Student>, abstract var lastName: String, var gender: String, var currentSubject: String) {
+
+    fun studentWithHighestGrade()  {
+        //TODO:
+
+    }
